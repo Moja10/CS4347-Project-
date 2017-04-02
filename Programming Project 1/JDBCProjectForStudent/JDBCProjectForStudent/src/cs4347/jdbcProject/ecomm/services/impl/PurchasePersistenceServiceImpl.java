2f7e99cebbe8e79c60cb232f318823d840e0235d
mathcoprocessor <z3r0n3@gmail.com>
2017-04-02 12:14:56 -0500

    Finished untested ServiceImpl
    Wrote connection/commit code for all methods in
    PurchasePersistenceServiceImpl.java, but I have no idea if any of them
    will work.  Mostly just copy/paste and changing return variables.

--- Programming Project 1/JDBCProjectForStudent/JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
+++ Programming Project 1/JDBCProjectForStudent/JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/services/impl/PurchasePersistenceServiceImpl.java
@@ -45,26 +45,140 @@ public class PurchasePersistenceServiceImpl implements PurchasePersistenceServic
 	}
 		
 	public Purchase retrieve(Long id) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			Purchase purch = PurchDAOImpl.retrieve(connection, id);
+			connection.commit();
+			return purch;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 	
 	public int update(Purchase purchase) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			int updateID = PurchDAOImpl.update(connection, purchase);
+			connection.commit();
+			return updateID;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 	
 	public int delete(Long id) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			int deleteID = PurchDAOImpl.delete(connection, purchase);
+			connection.commit();
+			return deleteID;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 	
 	public List<Purchase> retrieveForCustomerID(Long customerID) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			List<Purchase> listofpurch = PurchDAOImpl.retrieveForCustomerID(connection, customerID);
+			connection.commit();
+			return listofpurch;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 	
 	public PurchaseSummary retrievePurchaseSummary(Long customerID) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			PurchaseSummary result = PurchDAOImpl.retrievePurchaseSummary(connection, customerID);
+			connection.commit();
+			return result;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 	
 	public List<Purchase> retrieveForProductID(Long productID) throws SQLException, DAOException{
-		
+		PurchaseDAO PurchDAOImpl = new PurchaseDaoImpl();
+		Connection connection = dataSource.getConnection();
+		try{
+			connection.setAutoCommit(false);
+			List<Purchase> listofpurch = PurchDAOImpl.retrieveForProductID(connection, productID);
+			connection.commit();
+			return listofpurch;
+		}
+		catch (Exception ex){
+			// Rollback will set Autocommit back to true
+			connection.rollback();
+			throw ex;
+		}
+		finally{
+			// Autocommit is set back to true in the finally block
+			if (connection != null && !connection.isClosed()) {
+				connection.setAutoCommit(true);
+				connection.close();
+			}
+		}
 	}
 }
