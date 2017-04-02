52197f92cd59aad20da26a1eee60005be71429e5
mathcoprocessor <z3r0n3@gmail.com>
2017-04-01 22:15:28 -0500

    Finished implementing Purchase create DAO

--- Programming Project 1/JDBCProjectForStudent/JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
+++ Programming Project 1/JDBCProjectForStudent/JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
@@ -2,8 +2,9 @@ package cs4347.jdbcProject.ecomm.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.sql.Statement;
 
 import cs4347.jdbcProject.ecomm.dao.PurchaseDAO;
 import cs4347.jdbcProject.ecomm.entity.Purchase;
@@ -19,35 +20,58 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	 * value set by the application's auto-increment primary key column. 
 	 * @throws DAOException if the given Purchase has a non-null id.
 	 */
-	String insertString "
-	Purchase create(Connection connection, Purchase purchase) throws SQLException, DAOException{
-	// throw DAOException if the ID is not NULL 
+	
+	
+	public Purchase create(Connection connection, Purchase purchase) throws SQLException, DAOException
+	{
+		final String insertSQL = 
+				"INSERT INTO PURCHASE (customerID, productID, purchaseDate, purchaseAmount)"
+				+ "VALUES (?,?,?,?)";  
+		// throw DAOException if the ID is not NULL 
 		if (purchase.getId() != null){
 			throw new DAOException("Trying to insert Purchase with NON-NULL ID");
 		}
 		PreparedStatement ps = null;
 		try{
+			ps = connection.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
+			ps.setLong(1, purchase.getCustomerID());
+			ps.setLong(2, purchase.getProductID());
+			ps.setDate(3, purchase.getPurchaseDate());
+			ps.setDouble(4, purchase.getPurchaseAmount());
+
+			int res = ps.executeUpdate();
+			if(res != 1) {
+				throw new DAOException("Create Did Not Update Expected Number Of Rows");
+			}
 			
+			ResultSet keyRS = ps.getGeneratedKeys();
+			keyRS.next();
+			int lastKey = keyRS.getInt(1);
+			purchase.setId((long)lastKey);
+
 		}
 		finally{
-			
+			if (ps != null && !ps.isClosed()) {
+				ps.close();
+			}
 		}
+		return purchase;
 	}
-	Purchase retrieve(Connection connection, Long id) throws SQLException, DAOException{
+	public Purchase retrieve(Connection connection, Long id) throws SQLException, DAOException{
 	
 	/**
 	 * The update method must throw DAOException if the provided 
 	 * Purchase has a NULL id. 
 	 */
 	}
-	int update(Connection connection, Purchase purchase) throws SQLException, DAOException{
+	public int update(Connection connection, Purchase purchase) throws SQLException, DAOException{
 	
 	/**
 	 * The update method must throw DAOException if the provided 
 	 * ID is null. 
 	 */
 	}
-	int delete(Connection connection, Long id) throws SQLException, DAOException{
+	public int delete(Connection connection, Long id) throws SQLException, DAOException{
 	
 	/**
 	 * Retrieve purchases for the given customer id
@@ -68,5 +92,4 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	PurchaseSummary retrievePurchaseSummary(Connection connection, Long customerID) throws SQLException, DAOException{
 		
 	}
-}
 }
